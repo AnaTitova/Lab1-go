45c2806fbedc7952938e4131033755c7f31b71d2
Titova Anastasiya <nastyatitova266@gmail.com>
2018-11-27 19:15:13 +0300

    Update lab1.go

--- lab1.go
+++ lab1.go
@@ -3,14 +3,17 @@ package main
 import (
 	"archive/zip"
 	"bytes"
+	"crypto/sha1"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/binary"
 	"encoding/xml"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime/debug"
@@ -18,21 +21,44 @@ import (
 	"github.com/fullsailor/pkcs7"
 )
 
+//func init() {
+//
+//	flag.StringVar(&Mode, "mode", "", "Here you should place z - to zip, sz - to sertificate zip, u - to unzip")
+//	flag.StringVar(&Hash, "hash", "", "")
+//	flag.StringVar(&CertName, "cert", "./my.crt", "")
+//	flag.StringVar(&KeyName, "pkey", "./my.key", "")
+//	flag.StringVar(&Path, "path", "./", "")
+//	flag.StringVar(&Output, "out", "archive.szip", "")
+//}
+
+var crtLocation string = "./my.cer"
+var keyLocation string = "./my.key"
+
 func main() {
-	fmt.Println("test")
+	var hash string
+	flag.StringVar(&hash, "hash", "UNDEF", "hash")
+	fmt.Println("--------------------")
 	var mode string
 	flag.StringVar(&mode, "mode", "z", "режим работы приложения")
 	flag.Parse()
-	var err error
 	switch mode {
 	case "z":
-		{
-			err = prepareSzip()
-			break
-		}
+		err := prepareSzip()
 		if err != nil {
-			fmt.Printf("Ошибка: %s\nПричина:\n%s", err, debug.Stack())
+			fmt.Printf("Error occured: %s\nReason is here:\n%s", err, debug.Stack())
+			return
 		}
+		fmt.Println("Your archive has been successfuly Szpped")
+
+	case "i":
+		err := info(hash)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+	default:
+		fmt.Println("Uknown command. Please read manual and restart the application")
 	}
 }
 
@@ -64,18 +90,18 @@ func signData(data []byte) (sighed []byte, err error) {
 //------------------------------------------------------------------------------------
 
 func prepareSzip() (err error) {
-	var xml, metaZip, zipData []byte
+	var xMl, metaZip, zipData []byte
 	collector := NewFileCollector()
 	if err = collector.walkFiles("./test"); err != nil {
 		return
 	}
 
-	if xml, err = collector.meta2xml(); err != nil {
+	if xMl, err = collector.meta2xml(); err != nil {
 		return
 	}
 
 	metaCollector := NewFileCollector()
-	if err = metaCollector.packFile("meta.xml", bytes.NewReader(xml)); err != nil {
+	if err = metaCollector.packFile("meta.xml", bytes.NewReader(xMl)); err != nil {
 		return
 	}
 
@@ -95,9 +121,13 @@ func prepareSzip() (err error) {
 func makeSzip(metaZip, dataZip []byte) (err error) {
 	resultBuf := new(bytes.Buffer)
 
-	if err = binary.Write(resultBuf, binary.LittleEndian, uint32(len(metaZip))); err != nil {
+	metaSize := make([]byte, 4)
+	binary.LittleEndian.PutUint32(metaSize, uint32(len(metaZip)))
+	if err = binary.Write(resultBuf, binary.LittleEndian, metaSize); err != nil {
 		return
 	}
+
+	fmt.Println(len(metaZip))
 
 	if _, err = resultBuf.Write(metaZip); err != nil {
 		return
@@ -122,7 +152,11 @@ func makeSzip(metaZip, dataZip []byte) (err error) {
 
 //Единица передачи метаданных файла
 type FileMeta struct {
-	Name string `xml:"filename"`
+	Name           string `xml:"filename"`
+	OriginalSize   uint64 `xml:"original_size"`
+	CompressedSize uint64 `xml:"compressed_size"`
+	ModTime        string `xml:"mod_time"`
+	//Sha1Hash       [20]byte `xml:"sha1_hash"`
 }
 
 //------------------------------------------------------------------------------------
@@ -181,9 +215,9 @@ func (f *FileCollector) walkFiles(path string) (err error) {
 
 //------------------------------------------------------------------------------------
 
-func (f *FileCollector) meta2xml() (XML []byte, err error) {
-	XML, err = xml.Marshal(f.MetaData)
-	return XML, err
+func (f *FileCollector) meta2xml() (js []byte, err error) {
+	js, err = xml.Marshal(f.MetaData)
+	return js, err
 }
 
 //------------------------------------------------------------------------------------
@@ -221,3 +255,108 @@ func (f *FileCollector) zipData() (data []byte, err error) {
 }
 
 //------------------------------------------------------------------------------------
+
+func CheckSzp(szpLocation string, hash string) (error, *pkcs7.PKCS7) {
+	szp, err := ioutil.ReadFile(szpLocation)
+	if err != nil {
+		return err, nil
+	}
+
+	sign, err := pkcs7.Parse(szp)
+	if err != nil {
+		return err, nil
+	}
+
+	err = sign.Verify()
+	if err != nil {
+		return err, nil
+	}
+
+	signer := sign.GetOnlySigner()
+	if signer == nil {
+		return errors.New("Unable to obtain a single signer"), nil
+	}
+
+	if hash != "UNDEF" {
+		if hash != fmt.Sprintf("%x", sha1.Sum(signer.Raw)) {
+			fmt.Println(fmt.Sprintf("%x", sha1.Sum(signer.Raw)))
+			return errors.New("ERROR: Certificate hash is corrupted"), nil
+		}
+	}
+
+	crt, err := tls.LoadX509KeyPair(crtLocation, keyLocation)
+	if err != nil {
+		return err, nil
+	}
+
+	parsedCrt, err := x509.ParseCertificate(crt.Certificate[0])
+	if err != nil {
+		return err, nil
+	}
+
+	if bytes.Compare(parsedCrt.Raw, signer.Raw) != 0 {
+		return errors.New("Certificates don't match"), nil
+	}
+	return nil, sign
+}
+
+//------------------------------------------------------------------------------------
+
+func info(hash string) error {
+	err, sign := CheckSzp("./test.szp", hash)
+	if err != nil {
+		return err
+	}
+
+	err, fileMetas := GetMeta(sign)
+	if err != nil {
+		//fmt.Println("ошибка 1")
+		return err
+	}
+
+	fmt.Println(len(fileMetas))
+
+	for _, file := range fileMetas {
+		fmt.Println(file)
+	}
+
+	return err
+}
+
+//------------------------------------------------------------------------------------
+
+func GetMeta(p *pkcs7.PKCS7) (error, []FileMeta) {
+	//Read meta
+	metaSize := int32(binary.LittleEndian.Uint32(p.Content[:4]))
+	fmt.Println(metaSize)
+	bytedMeta := bytes.NewReader(p.Content[4 : metaSize+4])
+
+	readableMeta, err := zip.NewReader(bytedMeta, bytedMeta.Size())
+	if err != nil {
+		//fmt.Println("ошибка 2")
+		return err, nil
+	}
+
+	metaCompressed := readableMeta.File[0] //meta.xml
+
+	metaUncompressed, err := metaCompressed.Open()
+	if err != nil {
+		//fmt.Println("ошибка 3")
+		return err, nil
+	}
+	defer metaUncompressed.Close()
+
+	var fileMetas []FileMeta
+	metaUncompressedBody, err := ioutil.ReadAll(metaUncompressed)
+	if err != nil {
+		//fmt.Println("ошибка 4")
+		return err, nil
+	}
+	err = xml.Unmarshal(metaUncompressedBody, &fileMetas)
+	if err != nil {
+		//fmt.Println("ошибка 4")
+		return err, nil
+	}
+
+	return err, fileMetas
+}
